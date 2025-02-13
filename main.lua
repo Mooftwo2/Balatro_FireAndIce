3605f377c62720208fc09b4ed2a5b186f75d96ca
Mooftwo2 <lkordov@ualberta.ca>
2025-02-12 19:12:28 -0700

    basic joker implementation

---
+++ main.lua
@@ -0,0 +1,23 @@
+SMODS.Atlas{
+    key = 'Jokers',
+    path = 'Jokers.png',
+    px = 71,
+    py = 95
+}
+
+SMODS.Joker{
+
+    key = 'firejoker',
+    loc_txt = {
+
+        name = 'Flaming Joker',
+        text = {
+            'Triples effect of',
+            '{C:attention}Joker to the right,',
+            'disable it at end of round'
+
+        }
+    },
+    atlas = 'Jokers',
+    pos = {x= 0, y = 0}
+}
